8d9eba1658115cdbc978d6776bd76e65acbdd1d0
ryan <1184518756@qq.com>
2021-10-18 17:07:40 +0800

    1.0.1 beta

--- main/commom/async/synchronously.ts
+++ main/commom/async/synchronously.ts
@@ -1,49 +1,49 @@
-const syncQueueMap = {}
+const syncQueueMap = new Map();
 
 async function startExecSyncQueue(queue, ...args) {
-    let result
-    await Promise.resolve()
+    let result;
+    await Promise.resolve();
     for (let task = queue.shift(); queue.length >= 0 && task; task = queue.shift()) {
         if (typeof task === 'function') {
-            result = await task(...args)
+            result = await task(...args);
         }
     }
-    return result
+    return result;
 }
 
 function waitTaskExecute(task) {
-    let resultTask
+    let resultTask;
     const pr = new Promise(
         resolve => {
             resultTask = async (...args) => {
-                const result = await task(...args)
-                resolve(result)
-                return result
-            }
+                const result = await task(...args);
+                resolve(result);
+                return result;
+            };
         }
-    )
+    );
     return {
         promise: pr,
         task: resultTask
-    }
+    };
 }
 
-export async function synchronously(identity, ...tasks) {
+export async function synchronously(identity: any, ...tasks: ((...args: any[]) => any)[]) {
 
-    const existQueue = syncQueueMap[identity] || []
+    const existQueue = syncQueueMap.get(identity) || [];
 
-    const hasTasks = existQueue.length > 0
+    const hasTasks = existQueue.length > 0;
 
-    const taskMappings = tasks.filter(t => typeof t === 'function').map(t => waitTaskExecute(t))
+    const taskMappings = tasks.filter(t => typeof t === 'function').map(t => waitTaskExecute(t));
 
-    existQueue.push(...taskMappings.map(({ task }) => task))
+    existQueue.push(...taskMappings.map(({ task }) => task));
 
     if (!hasTasks) {
-        syncQueueMap[identity] = existQueue
-        startExecSyncQueue(existQueue)
+        syncQueueMap.set(identity, existQueue);
+        startExecSyncQueue(existQueue);
     }
 
-    const results = await Promise.all(taskMappings.map(({ promise }) => promise))
+    const results = await Promise.all(taskMappings.map(({ promise }) => promise));
 
-    return results[results.length - 1]
+    return results[results.length - 1];
 }
