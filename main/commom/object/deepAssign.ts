2afa3c498530f423689e4eb64177707152d75345
ryan <1184518756@qq.com>
2022-01-10 14:23:48 +0800

    beta9 update

--- main/commom/object/deepAssign.ts
+++ main/commom/object/deepAssign.ts
@@ -1,10 +1,18 @@
+import { isBasicData } from '../isBasicData';
+
 export function deepAssign(target, origin){
     for(let key in origin){
         if(origin.hasOwnProperty(key)){
-            if(typeof origin[key] === 'object'){
-                target[key] = deepAssign(target[key] || {}, origin[key]);
-            }else{
-                target[key] = origin[key];
+            const _value = origin[key];
+            if(isBasicData(_value)) {
+                target[key] = _value;
+            }else if (typeof _value === 'object') {
+                if(!target[key]){
+                    target[key] = Array.isArray(target[key]) ? [] : {};
+                }
+                deepAssign(target[key], _value);
+            } else {
+                target[key] = _value;
             }
         }
     }
